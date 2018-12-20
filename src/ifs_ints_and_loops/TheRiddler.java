7d22fcfad459cdbd5ef81fd6a235cea8dd57d5b9
ilabstaff <ilabstaff@172.16.116.73>
2018-12-19 19:22:41 -0800

    mod1

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -12,22 +12,25 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-int score= 0;
+		int score= 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-           String grinch=JOptionPane.showInputDialog(null, "What word begins with E and ends with E but only has one letter? " );
+        String grinch=JOptionPane.showInputDialog(null, "What word begins with E and ends with E but only has one letter? " );
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-if(grinch.equals("Envelope")) {
-	 score++;JOptionPane.showMessageDialog(null, "Correct");
-}
+        if(grinch.equals("Envelope")) {
+        	score++;
+        	JOptionPane.showMessageDialog(null, "Correct");
+        }
 
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+        else {
+        	JOptionPane.showMessageDialog(null," Wrong, The grinch will get you!");
+        }
 
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, score);
 	}
 }
 

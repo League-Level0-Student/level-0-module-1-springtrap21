b4510094c3a69d0ca2058595e540ce49b2bf7224
s.weise <s.weise@sw-pc>
2019-01-15 17:02:31 -0800

    myages

--- src/myages.java
+++ src/myages.java
@@ -1,8 +1,10 @@
 
     public class myages {
     public static void main(String[] args) {
-	for (int i = 0; i < 11; i++) {
-	System.out.println(i);
+    	int born = 2010;
+    	int year = 2019;
+	for (int i = born; i < year; i++) {
+	System.out.println(i - born);
 	}
     }
     }
